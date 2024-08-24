982e70a2f5fc3b366876ba253ad2fd0247847297
Dez Xerneas <sarth.bhagat@gmail.com>
2024-08-24 11:24:52 +0530

    update

--- .config/fish/functions/lss.fish
+++ .config/fish/functions/lss.fish
@@ -1,4 +1,4 @@
-function lss --wraps='exa -lahF --icons --color always --group-directories-first -s modified -r' --description 'alias lsm exa -lahF --icons --color always --group-directories-first -s modified -r'
-  exa -lahF --icons --color always --group-directories-first -s modified -r $argv
+function lss --wraps='eza -lahF --icons --color always --group-directories-first -s modified -r' --description 'alias lsm eza -lahF --icons --color always --group-directories-first -s modified -r'
+  eza -lahF --icons --color always --group-directories-first -s modified -r $argv
         
 end
--- .config/fish/private.fish
+++ .config/fish/private.fish
@@ -1,2 +1,3 @@
 #set -gx fish_user_paths "$HOME/.rye/shims"
 source (/usr/bin/starship init fish --print-full-init | psub)
+source ~/.asdf/asdf.fish
