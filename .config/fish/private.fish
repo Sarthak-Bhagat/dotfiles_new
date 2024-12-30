c489ce4ff00b92fac9366e9e2f48285834b18047
Sarthak Bhagat <sarth.bhagat@gmail.com>
2024-12-30 19:25:34 +0530

    update

--- .config/fish/private.fish
+++ .config/fish/private.fish
@@ -3,3 +3,7 @@ source (/usr/bin/starship init fish --print-full-init | psub)
 set -Ua fish_user_paths "$HOME/.rye/shims"
 umask 002
 ufetch
+#if status is-interactive
+#    and not set -q TMUX
+#    exec tmux -vvuf /home/dez/.config/tmux/tmux.conf
+#end
